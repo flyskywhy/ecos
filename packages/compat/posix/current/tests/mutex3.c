03a0bb051cdfb4a14a45769f43b508c0cc01b189
jlarmour <devnull@localhost>
2003-03-03 16:49:08 +0000

    Also include <unistd.h> for proper feature test detection

--- packages/compat/posix/current/tests/mutex3.c
+++ packages/compat/posix/current/tests/mutex3.c
@@ -56,6 +56,14 @@
 #include <pkgconf/kernel.h>
 #endif
 
+#ifdef CYGPKG_ISOINFRA
+# include <sys/types.h>
+# include <pthread.h>
+# include <semaphore.h>
+# include <time.h>
+# include <unistd.h>
+#endif
+
 #if !defined(CYGPKG_POSIX_PTHREAD)
 #define NA_MSG "POSIX threads not enabled"
 
@@ -85,11 +93,6 @@ cyg_start(void)
     CYG_TEST_NA(NA_MSG);
 }
 #else
-
-#include <sys/types.h>
-#include <pthread.h>
-#include <semaphore.h>
-#include <time.h>
 
 #include <cyg/infra/cyg_ass.h>
 #include <cyg/infra/cyg_trac.h>
