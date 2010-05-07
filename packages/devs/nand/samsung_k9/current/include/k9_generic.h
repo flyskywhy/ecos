a20af10052ecea149bc846c435103f9e2f830682
Ross Younger <wry@ecoscentric.com>
2010-05-07 11:54:23 +0100

    Update samsung_k9 to latest version

--- packages/devs/nand/samsung_k9/current/include/k9_generic.h
+++ packages/devs/nand/samsung_k9/current/include/k9_generic.h
@@ -1,16 +1,16 @@
-#ifndef CYGONCE_K9FXX08X0X_H
-#define CYGONCE_K9FXX08X0X_H
+#ifndef CYGONCE_K9_GENERIC_H
+#define CYGONCE_K9_GENERIC_H
 //=============================================================================
 //
-//      k9fxx08x0x.h
+//      k9_generic.h
 //
-//      Definitions header for the Samsung K9Fxx08x0x family
+//      Definitions header for the Samsung K9F family
 //
 //=============================================================================
 // ####ECOSGPLCOPYRIGHTBEGIN####                                            
 // -------------------------------------------                              
 // This file is part of eCos, the Embedded Configurable Operating System.   
-// Copyright (C) 2009 eCosCentric Limited.
+// Copyright (C) 2009,2010 eCosCentric Limited.
 //
 // eCos is free software; you can redistribute it and/or modify it under    
 // the terms of the GNU General Public License as published by the Free     
@@ -44,39 +44,36 @@
 // Author(s):   wry
 // Date:        2009-06-05
 // Description: Early definitions for the driver.
-//              The intention is that the instantiating C file include
-//              this file once, define the things it needs to, then
-//              include k9fxx08x0x.inl as many times as necessary
-//              (probably just once).
 //
 //####DESCRIPTIONEND####
 //=============================================================================
 
 #include <pkgconf/io_nand.h>
+#include <pkgconf/devs_nand_samsung_k9.h>
 #include <cyg/nand/nand_device.h>
 #include <cyg/hal/drv_api.h>
 #include <cyg/infra/diag.h>
 
 #define k9_WAIT_tWB() HAL_DELAY_US(1)
 
-// This is a 1Gbit device with 1024 eraseblocks.
-#define k9f1g_blockcount_bits 10
-#define k9f1g_chipsize_log  27 // 1Gbit = 2^30 bits = 2^27 bytes
-
-// The in-RAM BBT for a device is currently 2 bits per eraseblock.
-// See nand_bbt.c for more.
-#define k9f1g_bbt_datasize (1<<(k9f1g_blockcount_bits-2))
-
 /* Our private structure ======================================= */
 // Every instance of the chip needs its own copy of this struct.
 // N.B. that this is too big to go on the stack in certain
 // eCos configurations; it should normally be static.
 
+struct _k9_subtype;
+typedef struct _k9_subtype k9_subtype;
+
 struct _k9_priv {
+    k9_subtype *supported_chips; // Must be set by the platform HAL. Terminate with a zero-entry! See K9_SUPPORT_LIST below.
     void *plat_priv; // For use by the platform HAL, if desired.
+    // ------------ //
     cyg_nand_page_addr pagestash; // Guarded by dev lock.
+    CYG_BYTE row_addr_size; // Set up by devinit after chip detection.
 #ifdef CYGSEM_IO_NAND_USE_BBT
-    unsigned char bbt_data[k9f1g_bbt_datasize];
+// The in-RAM BBT for a device is fixed at 2 bits per eraseblock.
+// See nand_bbt.c for more.
+    unsigned char bbt_data[CYGNUM_DEVS_NAND_SAMSUNG_K9_BBT_DATASIZE];
 #endif
 };
 
@@ -87,7 +84,9 @@ typedef struct _k9_priv k9_priv;
 // Low-level chip access functions ------------------
 
 static inline void write_cmd(cyg_nand_device *ctx, unsigned char cmd);
+
 static inline void write_addrbytes(cyg_nand_device *ctx, CYG_BYTE *bytes, size_t n);
+
 static inline unsigned char read_data_1(cyg_nand_device *ctx);
 static inline void read_data_bulk(cyg_nand_device *ctx, unsigned char *dp, size_t n);
 static inline void write_data_1(cyg_nand_device *ctx, unsigned char b);
@@ -133,5 +132,33 @@ static int k9_plf_init(cyg_nand_device *ctx);
  * (This is called at the end of devinit, so you can read from the chip
  * if need be.) */
 static int k9_plf_partition_setup(cyg_nand_device *dev);
+
+// Internal entrypoints (used by some large page ECC drivers)
+#ifdef CYGFUN_NAND_SAMSUNG_K9_LP
+static inline void change_read_column_lp(cyg_nand_device *dev, cyg_nand_column_addr col);
+#endif
+
+// Family support -----------------------------------
+
+// Basic definitions for k9 subtypes
+
+struct _k9_subtype {
+    CYG_BYTE ident1, ident3;
+    const char *descriptor;
+    CYG_BYTE blockcount_bits;
+    CYG_BYTE chipsize_log;
+    CYG_BYTE row_address_size;
+    CYG_BYTE sp; // set non-0 if device is small-page
+};
+
+// Callers should not use K9_SUBTYPE directly.
+// Instead the known subtypes are defined in k9_generic.inl.
+#define K9_SUBTYPE(id1,id3,desc,bcb,csl,ras) { id1, id3, desc, bcb, csl, ras, 0 }
+#define K9_SUBTYPE_SP(id1,id3,desc,bcb,csl,ras) { id1, id3, desc, bcb, csl, ras, 1 }
+
+// A list of supported subtypes must be zero-terminated!
+#define K9_SUBTYPE_SENTINEL { 0, 0, 0, 0, 0, 0, 0 }
+
+// For a list of implemented subtypes, refer to the bottom of k9_generic.inl.
 
 #endif
