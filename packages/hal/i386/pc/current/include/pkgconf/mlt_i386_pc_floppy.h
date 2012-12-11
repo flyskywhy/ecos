238e176f3549671db990ef511e2a80059b15f401
Li Zheng <flyskywhy@gmail.com>
2012-12-11 10:23:53 +0800

    i386: vmware like ram size 0x1000000 more than 0xa0000 to load elf in redboot

--- packages/hal/i386/pc/current/include/pkgconf/mlt_i386_pc_floppy.h
+++ packages/hal/i386/pc/current/include/pkgconf/mlt_i386_pc_floppy.h
@@ -8,13 +8,13 @@
 
 #endif
 #define CYGMEM_REGION_ram (0)
-#define CYGMEM_REGION_ram_SIZE (0xa0000)
+#define CYGMEM_REGION_ram_SIZE (0x1000000)
 #define CYGMEM_REGION_ram_ATTR (CYGMEM_REGION_ATTR_R | CYGMEM_REGION_ATTR_W)
 #ifndef __ASSEMBLER__
 extern char CYG_LABEL_NAME (__heap1) [];
 #endif
 #define CYGMEM_SECTION_heap1 (CYG_LABEL_NAME (__heap1))
-#define CYGMEM_SECTION_heap1_SIZE (0xa0000 - (size_t) CYG_LABEL_NAME (__heap1))
+#define CYGMEM_SECTION_heap1_SIZE (0x1000000 - (size_t) CYG_LABEL_NAME (__heap1))
 #ifndef __ASSEMBLER__
 extern char CYG_LABEL_NAME (__pci_window) [];
 #endif
