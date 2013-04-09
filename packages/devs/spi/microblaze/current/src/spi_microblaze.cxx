218f4683ea50607bd41ecb5d11c0dac01b2a7eb2
Li Zheng <flyskywhy@gmail.com>
2013-04-09 14:52:57 +0800

    Microblaze SPI driver: Disk driver for MMC cards need 0xff tick to make mmc_spi_send_init() useful, otherwise even MMC_REQUEST_GO_IDLE_STATE will fail

--- packages/devs/spi/microblaze/current/src/spi_microblaze.cxx
+++ packages/devs/spi/microblaze/current/src/spi_microblaze.cxx
@@ -256,7 +256,7 @@ spi_microblaze_transfer(cyg_spi_device *device, cyg_bool polled, cyg_uint32 coun
 static void
 spi_microblaze_tick(cyg_spi_device *device, cyg_bool polled, cyg_uint32 count)
 {
-  cyg_uint8 tx_zero[10] = {0,0,0,0,0,0,0,0,0,0};
+  cyg_uint8 tx_zero[10] = {0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff};
   spi_microblaze_transfer(device, polled, count, tx_zero, NULL,false);
 }
 
